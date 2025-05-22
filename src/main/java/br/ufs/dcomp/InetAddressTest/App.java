88d66cfca27d7c48f84ddeddb06426703664433c
Ubuntu <ubuntu@ip-172-31-32-38.ec2.internal>
2025-05-22 19:28:08 +0000

    Novo início limpo do projeto

---
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -0,0 +1,23 @@
+package br.ufs.dcomp.InetAddressTest;
+
+import java.net.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try{
+            
+            String name = "google.com.br";
+            InetAddress address = InetAddress.getByName(name); 
+            System.out.println( "Name:      "+ name);
+            System.out.println( "Address:   "+ address.getHostAddress());
+        } catch (UnknownHostException e){
+            System.out.println(e.getMessage());
+        }                
+    }
+}
